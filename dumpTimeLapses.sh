5feb6ba1c7a816687de26f541a2e03cfcc89393e
JGWill-Orko <jgi@jgwill.com>
2021-06-01 18:46:25 -0400

    feat: Works -Sounds like I got something

--- dumpTimeLapses.sh
+++ dumpTimeLapses.sh
@@ -28,28 +28,31 @@ rtmpdump --stop 2 \
     -o $OUTDIR/1.flv &> /dev/null &
 rpid=$! 
 echo -n "." ; sleep 1 ; echo -n "."; sleep 1 ; echo -n "."; sleep 1 ; echo -n "."
-sleep 5
+sleep 8
 echo "------------------------------------------------------"
-kill -9 $rpid  &> /dev/null && echo "GOT KILLED " && sleep 1 || echo "Can kill anyone"  
-echo "------------------------------------------------------"
-#&> /dev/null
-
-cframe="$OUTDIR/$TIME.jpg"; echo "cframe: $cframe"
-ffmpeg -i $OUTDIR/1.flv \
+kill -9 $rpid &> /dev/null \
+&& echo "GOT KILLED " \
+ && \
+echo "------------------------------------------------------" && \
+\
+cframe="$OUTDIR/$TIME.jpg"; echo "cframe: $cframe" &&\
+(ffmpeg -i $OUTDIR/1.flv \
     -ss 00:00:02 -an \
     -r 1 -vframes 1 \
     -s 1280x860 \
-    -y "$cframe" &> /dev/null && echo "--------FFMPEG OK " && \
+    -y "$cframe" &> /dev/null && \
+   ( cp $cframe $OUTDIR/_current.jpg || echo "Error with Copying Frame :((")  &> /dev/null && \
+    echo -n "--------FFMPEG OK ..." && \
      sleep 6 \
-    || echo "----FFMPEG NOT OK ---"
+    || echo -n "----FFMPEG NOT OK ---") \
+|| (echo "Can kill anyone" &&  kill -9 $rpid  &> /dev/null)
 
-cp $cframe $OUTDIR/_current.jpg  &> /dev/null
 
 
 echo "Waiting until next run..."; echo -n "." ; sleep 1 ; echo -n "."
 #sleep 4
 
-$0
+$0 &
 
 
 exit;
