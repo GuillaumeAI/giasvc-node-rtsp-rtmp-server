3e029b27d84b0f4b3905d3a8fefd0d455ce395f6
JGWill-Orko <jgi@jgwill.com>
2021-06-01 18:33:40 -0400

    hummm

--- dumpTimeLapses.sh
+++ dumpTimeLapses.sh
@@ -20,14 +20,36 @@ TIME=$(date +"%Y%m%d_%H%M%S")
 
 mkdir -p $OUTDIR
 
-rtmpdump -v -r $RTMPURL -y $STREAM -o $OUTDIR/1.flv -B 1
-ffmpeg -i $OUTDIR/1.flv -ss 00:00:01 -an -r 1 -vframes 1 -s 800x600 -y $OUTDIR/$TIME.jpg
+#rtmpdump -v -r $RTMPURL -y $STREAM -o $OUTDIR/1.flv -B 1
+rm $OUTDIR/1.flv  &> /dev/null
+rtmpdump --stop 2 \
+    -v \
+    -r $RTMPURL  \
+    -o $OUTDIR/1.flv &> /dev/null &
+rpid=$! 
+echo -n "." ; sleep 1 ; echo -n "."; sleep 1 ; echo -n "."; sleep 1 ; echo -n "."
+sleep 5
+echo "------------------------------------------------------"
+kill -9 $rpid  &> /dev/null && echo "GOT KILLED " && sleep 1 || echo "Can kill anyone"  
+echo "------------------------------------------------------"
+#&> /dev/null
+
+cframe="$OUTDIR/$TIME.jpg"; echo "cframe: $cframe"
+ffmpeg -i $OUTDIR/1.flv \
+    -ss 00:00:02 -an \
+    -r 1 -vframes 1 \
+    -s 1280x860 \
+    -y "$cframe" &> /dev/null && echo "--------FFMPEG OK " && \
+     sleep 6 \
+    || echo "----FFMPEG NOT OK ---"
+
+cp $cframe $OUTDIR/_current.jpg  &> /dev/null
 
 
-echo "Waiting until next run..."
-sleep 10
+echo "Waiting until next run..."; echo -n "." ; sleep 1 ; echo -n "."
+#sleep 4
 
-./cap.sh
+$0
 
 
 exit;
